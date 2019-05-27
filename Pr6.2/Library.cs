ee2762dcbf867b4e09cfc2f1d6114464bfd38132
KozlovaDasha <dashakozlova255@ukr.net>
2019-05-27 18:00:59 +0300

    Добавьте файлы проекта.

---
+++ Pr6.2/Library.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr6
+{
+    class Library : Book
+    {
+        public Book[] books;
+
+        public Library(int amountOfBooks = 2)
+        {
+            books = new Book[amountOfBooks];
+        }
+
+        public void CompareTwoBooks()
+        {
+            Console.WriteLine("_________________COMPARING BOOKS________________");
+
+            uint firstBookNumber = new uint();
+            do {
+                Console.WriteLine("Enter number of first book: ");
+                while (uint.TryParse(Console.ReadLine(), out firstBookNumber) == false)
+                {
+                    Console.WriteLine("Wrong value! Try again: ");
+                }
+            } while (firstBookNumber >= books.Count());
+
+            uint secondBookNumber = new uint();
+            do
+            {
+                Console.WriteLine("Enter number of second book: ");
+                while (uint.TryParse(Console.ReadLine(), out secondBookNumber) == false)
+                {
+                    Console.WriteLine("Wrong value! Try again: ");
+                }
+            } while (secondBookNumber >= books.Count() || secondBookNumber == firstBookNumber);
+
+            Console.WriteLine("Enter number for comparing books:\n" +"1 - Name of author\n2 - Title of the book\n3 - Code\n4 - Year of writing" +
+                "\n5 - Amount of pages\n6 - Genre");
+
+            int choice = new int();
+            while (int.TryParse(Console.ReadLine(), out choice) == false)
+            {
+                Console.WriteLine("Wrong value! Try again: ");
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"Author of the first book: {books[firstBookNumber].FullAuthorName}\nAuthor of the second book: {books[secondBookNumber].FullAuthorName}");
+                    break;
+                case 2:
+                    Console.WriteLine($"First book name: {books[firstBookNumber].BookName}\nSecond book name: {books[secondBookNumber].BookName}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Code of the first book: {books[firstBookNumber].BookCode}\nCode of the second book: {books[secondBookNumber].BookCode}");
+                    break;
+                case 4:
+                    Console.WriteLine($"First book was written in the {books[firstBookNumber].YearOfWriting} year;\nSecond book was written in the {books[secondBookNumber].YearOfWriting} year");
+                    break;
+                case 5:
+                    Console.WriteLine($"First book has {books[firstBookNumber].AmountOfPages} pages;\n Second book has {books[secondBookNumber].AmountOfPages} pages");
+                    break;
+                case 6:
+                    Console.WriteLine($"Genre of the first book is {books[firstBookNumber].Genre};\n Genre of the second book is {books}");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+            }
+
+        }
+
+        public void SearchByCode()
+        {
+            bool Value = true;
+            Console.WriteLine("Enter searched code: ");
+            int searchedCode = new int();
+            while (int.TryParse(Console.ReadLine(), out searchedCode) == false)
+            {
+                Console.Write("Wrong value. Try again: ");
+            }
+
+            foreach (var book in books)
+            {
+                if(book.BookCode == searchedCode)
+                {
+                    Console.WriteLine(book.ToString());
+                    Value = false;
+                }
+            }
+            if (Value)
+            {
+                Console.WriteLine("There is no such books!");
+            }
+        }
+    }
+}
---
+++ Pr6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library(5);
+            library.books[0] = new Book("J.K. Rowling", "Harry Potter", 2035, 1997, 522, "Fantasy");
+            library.books[1] = new Book(" Margaret Brown", "Goodnight Moon", 1042, 1851, 746, "Novel");
+            library.books[2] = new Book("Margret Rey", "Curious George", 4234, 1605, 1023, "Novel");
+            library.books[3] = new Book("Leo Tostoy", "Anna Karenina", 9431, 1898, 1402, "Tragedy");
+            library.books[4] = new Book("Shel Silverstein", "The Giving Tree", 1234, 1930, 416, "Fairytail");
+
+            foreach (var book in library.books)
+            {
+                Console.WriteLine(book.ToString());
+            }
+
+            library.CompareTwoBooks();
+            library.SearchByCode();
+
+            Console.ReadLine();
+        }
+    }
+}
